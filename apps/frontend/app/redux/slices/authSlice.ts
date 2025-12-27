793f197a42821673c7ca2b2db555f8d8c5baa339
ElormLokpo <benedictdev31@gmail.com>
2025-12-27 06:55:50 +0000

    destructure token to redux

--- apps/frontend/app/redux/slices/authSlice.ts
+++ apps/frontend/app/redux/slices/authSlice.ts
@@ -1,11 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { IUserRedux } from "../../../../../packages/types";
 
 interface AuthState {
     token:string | undefined,
+    user: IUserRedux | undefined
 }
 
 const initialState: AuthState = {
     token: undefined,
+    user: undefined
 }
 
 export const authSlice = createSlice({
@@ -17,9 +20,16 @@ export const authSlice = createSlice({
         },
         removeToken: (state) => {
             state.token = undefined
+        },
+        setUser: (state, action) => {
+            state.user = action.payload
+        },
+        removeUser: (state) => {
+            state.user = undefined
         }
+
     }
 })
 
-export const { setToken, removeToken } = authSlice.actions
+export const { setToken, removeToken, setUser, removeUser } = authSlice.actions
 export default authSlice.reducer
